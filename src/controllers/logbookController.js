8c25649be3e14b9a0efd1612a30ee85e02f2d706
rozakrmdhn <abdulrozakramadhoni@gmail.com>
2024-10-17 21:56:24 +0700

    update logbook

--- src/controllers/logbookController.js
+++ src/controllers/logbookController.js
@@ -206,59 +206,76 @@ const getLogbookByMahasiswa = async (request, h) => {
 const getLogbookMahasiswa = async (request, h) => {
     const { id_mahasiswa } = request.payload;
     try {
-        const results = await Logbook.findAll({
-            order: [['tanggal', 'ASC']],
+        const cekAnggota = await Anggota.findAll({
+            attributes: ['id', 'id_mahasiswa'],
             include: [
-                { 
-                    model: Anggota, 
-                    as: 'anggota', 
-                    attributes: ['id_mahasiswa', 'id_daftar'],
-                    include: [
-                        {
-                            model: Daftar,
-                            as: 'daftar',
-                            attributes: ['tempat_kp', 'alamat', 'kota', 'tanggal_kp'],
-                            include: [
-                                {
-                                    model: Dosen,
-                                    as: 'dosen',
-                                    attributes: ['id','nip','nama']
-                                }
-                            ]
-                        },
-                        {
-                            model: Mahasiswa,
-                            as: 'mahasiswa',
-                            attributes: ['id','nrp','nama'],
-                        },
-                    ],
-                    where: { id_mahasiswa: id_mahasiswa }
+                {
+                    model: Daftar,
+                    as: 'daftar',
+                    attributes: ['status_persetujuan'],
+                    where: {
+                        status_persetujuan: 1
+                    }
                 }
             ],
+            where: {
+                id_mahasiswa: id_mahasiswa
+            }
         });
 
-        // Formatting of the response data
-        const formattedResults = results.map(logbook => ({
-            id: logbook.id,
-            id_mahasiswa: logbook.anggota.mahasiswa.id,
-            nama: logbook.anggota.mahasiswa.nama,
-            id_dosen: logbook.anggota.daftar.dosen.id,
-            dosen_pembimbing: logbook.anggota.daftar.dosen.nama,
-            tanggal: logbook.tanggal,
-            jam_mulai: logbook.jam_mulai,
-            jam_selesai: logbook.jam_selesai,
-            kegiatan: logbook.kegiatan,
-            kesesuaian_matkul_diajarkan: logbook.kesesuaian_matkul_diajarkan,
-            matkul_diajarkan: logbook.matkul_diajarkan,
-            setujui_logbook: logbook.setujui_logbook,
-            lampiran_laporan: logbook.lampiran_laporan,
-            lampiran_foto: logbook.lampiran_foto,
-            catatan_pembimbing: logbook.catatan_pembimbing,
-            createdAt: logbook.createdAt,
-            updatedAt: logbook.updatedAt
-        }));
+        if (cekAnggota.length === 1) {
+            const results = await Logbook.findAll({
+                order: [['tanggal', 'ASC']],
+                include: [
+                    { 
+                        model: Anggota, 
+                        as: 'anggota', 
+                        attributes: ['id_mahasiswa', 'id_daftar'],
+                        include: [
+                            {
+                                model: Daftar,
+                                as: 'daftar',
+                                attributes: ['tempat_kp', 'alamat', 'kota', 'tanggal_kp'],
+                                include: [
+                                    {
+                                        model: Dosen,
+                                        as: 'dosen',
+                                        attributes: ['id','nip','nama']
+                                    }
+                                ]
+                            },
+                            {
+                                model: Mahasiswa,
+                                as: 'mahasiswa',
+                                attributes: ['id','nrp','nama'],
+                            },
+                        ],
+                        where: { id_mahasiswa: id_mahasiswa }
+                    }
+                ],
+            });
 
-        if (results.length != 0) {
+            // Formatting of the response data
+            const formattedResults = results.map(logbook => ({
+                id: logbook.id,
+                id_mahasiswa: logbook.anggota.mahasiswa.id,
+                nama: logbook.anggota.mahasiswa.nama,
+                id_dosen: logbook.anggota.daftar.dosen.id,
+                dosen_pembimbing: logbook.anggota.daftar.dosen.nama,
+                tanggal: logbook.tanggal,
+                jam_mulai: logbook.jam_mulai,
+                jam_selesai: logbook.jam_selesai,
+                kegiatan: logbook.kegiatan,
+                kesesuaian_matkul_diajarkan: logbook.kesesuaian_matkul_diajarkan,
+                matkul_diajarkan: logbook.matkul_diajarkan,
+                setujui_logbook: logbook.setujui_logbook,
+                lampiran_laporan: logbook.lampiran_laporan,
+                lampiran_foto: logbook.lampiran_foto,
+                catatan_pembimbing: logbook.catatan_pembimbing,
+                createdAt: logbook.createdAt,
+                updatedAt: logbook.updatedAt
+            }));
+            
             return response = h.response({
                 status: 'success',
                 message: 'Berhasil mengambil data',
@@ -267,10 +284,10 @@ const getLogbookMahasiswa = async (request, h) => {
         } else {
             return response = h.response({
                 status: 'success',
-                message: 'Data Logbook tidak ditemukan',
-                data: formattedResults,
-            }).code(404);
+                message: 'Kesalahan mengambil Data Logbook'
+            }).code(400);
         }
+
     } catch (err) {
         console.log(err);
     }
